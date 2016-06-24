01495d87f14fc9fc90943b93cc2f8552b197e803
prashanth <vprashanth34@gmail.com>
2016-06-24 10:25:23 -0400

    1st commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBowerKarmaYoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularBowerKarmaYoApp
+ */
+angular.module('angularBowerKarmaYoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBowerKarmaYoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularBowerKarmaYoApp
+ */
+angular.module('angularBowerKarmaYoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
